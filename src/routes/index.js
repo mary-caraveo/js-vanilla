078ec0b9fd3d923fdf478300e33490895523adff
Guadalupe Caraveo <mary.caraveo94@gmail.com>
2021-08-11 13:01:54 -0500

    feat: Conexión de las rutas con los templates

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,9 @@ import header from "../templates/header";
 import home from "../pages/home";
 import characters from "../pages/character";
 import error404 from "../pages/error404";
+import getHash from "../utils/getHash";
+import resolveRoutes from "../utils/resolveRoutes";
+
 
 const routes = {
   "/": home,
